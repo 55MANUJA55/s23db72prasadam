5721c9705ec0da12c261b74ad6c046c98e4d151f
55MANUJA55 <manujaprasadam@gmail.com>
2023-11-10 10:56:24 -0600

    lab part completed

--- controllers/Badminton.js
+++ controllers/Badminton.js
@@ -12,11 +12,41 @@ exports.Badminton_list = async function (req, res) {
 
 
 
-
-
-exports.Badminton_detail = function(req, res) {
-  res.send('NOT IMPLEMENTED: Badminton detail: ' + req.params.id);
+// for a specific Badminton.
+exports.Badminton_detail = async function(req, res) {
+ console.log("detail" + req.params.id)
+ try {
+ result = await BadmintonModel.findById( req.params.id)
+ console.log(result)
+ res.send(result)
+ } catch (error) {
+ res.status(500)
+ res.send(`{"error": document for id ${req.params.id} not found`+error);
+ }
 };
+
+//Handle Costume update form on PUT.
+exports.Badminton_update_put = async function(req, res) {
+  console.log(`update on id ${req.params.id} with body 
+ ${JSON.stringify(req.body)}`)
+  try {
+  let toUpdate = await BadmintonModel.findById( req.params.id)
+  // Do updates of properties
+  if(req.body.Player_Name) 
+  toUpdate.Player_Name = req.body.Player_Name;
+  if(req.body.Player_Age) toUpdate.Player_Age = req.body.Player_Age;
+  if(req.body.No_Of_Matches_Played) toUpdate.No_Of_Matches_Played = req.body.No_Of_Matches_Played;
+  let result = await toUpdate.save();
+  console.log("Sucess " + result)
+  res.send(result)
+  } catch (err) {
+  res.status(500)
+  res.send(`{"error": ${err}: Update for id ${req.params.id} 
+ failed`);
+  }
+ };
+
+
 
 
 exports.Badminton_create_post =async  function(req, res) {
@@ -40,7 +70,4 @@ exports.Badminton_create_post =async  function(req, res) {
 
 exports.Badminton_delete = function(req, res) {
   res.send('NOT IMPLEMENTED: Badminton delete DELETE ' + req.params.id);
-};
-exports.Badminton_update_put = function(req, res) {
-  res.send('NOT IMPLEMENTED: Badminton update PUT ' + req.params.id);
 };
